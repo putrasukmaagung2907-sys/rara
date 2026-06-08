b8f387500b9981a22ab3c88a5a8aeba53a6f0f37
putrasukmaagung2907-sys <putrasukmaagung2907@gmail.com>
2026-06-08 18:34:20 +0700

    memperbarui script utama dan menambah aset baru

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "rara-playlist-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
